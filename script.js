3ee41bb61caaf3eba8f177f71f0ce09b5c3ed9a6
Dmitry Gorbach <89874530087@mail.ru>
2022-04-08 00:41:09 +0200

    add lesson11

--- script.js
+++ script.js
@@ -1,43 +1,30 @@
-const bookList = document.querySelectorAll(".books");
-const lists = document.querySelectorAll(".book");
-console.log(bookList);
-console.log(lists);
+"use strict";
+const buttonTop = document.getElementById("btn");
+const buttonBottom = document.getElementById("e_btn");
+const input = document.getElementById("text");
+const inputRange = document.getElementById("range");
+const circle = document.getElementById("circle");
+console.dir(circle);
 
-//1
-lists[0].before(lists[1]);
-bookList[0].append(lists[2]);
-lists[4].after(lists[3]);
+const clickFunc = function () {
+  buttonTop.style.backgroundColor = input.value;
+  /* console.log(buttonTop.style.backgroundColor);
+  console.dir(input.value); */
+};
 
-//2
-const body = document.querySelector("body");
-//console.dir(body);
+const clickFuncBottom = function (event) {
+  buttonBottom.style.display = "none";
+  /* console.log(event.target);
+  console.log(event.currenTarget); */
+};
 
-body.style.backgroundImage = "url(./image/you-dont-know-js.jpg)";
+const rangeFunc = function (event) {
+  circle.style.width = inputRange.value + "%";
+  circle.style.height = inputRange.value + "%";
+  /* console.log(circle.style.width);
+  console.log(inputRange.value); */
+};
 
-//3
-let title = lists[4].childNodes[1];
-title.innerText = "Книга 3. this и Прототипы Объектов";
-console.log(title.innerText);
-
-//4
-const advItem = document.querySelector(".adv");
-advItem.style.display = "none";
-//console.dir(advItem);
-
-//5
-//console.log(lists[0].childNodes[3].children);
-lists[0].childNodes[3].children[3].after(lists[0].childNodes[3].children[6]);
-lists[0].childNodes[3].children[5].before(lists[0].childNodes[3].children[8]);
-
-//console.log(lists[5].childNodes[3].children);
-lists[5].childNodes[3].children[1].after(lists[5].childNodes[3].children[9]);
-lists[5].childNodes[3].children[4].after(lists[5].childNodes[3].children[3]);
-lists[5].childNodes[3].children[3].after(lists[5].childNodes[3].children[5]);
-
-//6
-//console.dir(lists[2].childNodes[3].children);
-const newElem = document.createElement("li");
-newElem.textContent = "Глава 8: За пределами ES6";
-//console.log(newElem);
-lists[2].childNodes[3].append(newElem);
-lists[2].childNodes[3].children[8].after(lists[2].childNodes[3].children[10]);
+buttonTop.addEventListener("click", clickFunc);
+buttonBottom.addEventListener("click", clickFuncBottom);
+inputRange.addEventListener("input", rangeFunc);
